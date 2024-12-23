0f442392634ac6ee9c8403ff0302a0af2ba5db13
saini <sainii.3013@gmail.com>
2024-12-23 11:11:45 -0700

    Your commit message here

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port=3000
+const app = express();
+app.use(bodyParser.json());
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Hello, World!')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
